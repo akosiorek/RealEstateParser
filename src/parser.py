e628004b6720d2901b34dcb0ac3fd62e50928140
Adam Kosiorek <kosiorek.adam@gmail.com>
2015-03-04 13:19:39 +0100

    add encoding support

--- src/parser.py
+++ src/parser.py
@@ -2,6 +2,7 @@ __author__ = 'Adam Kosiorek'
 # −*− coding: UTF−8 −*−
 
 import types
+import sys
 
 DEBUG = False
 
@@ -19,15 +20,16 @@ class NoSuchParserException(Exception): pass
 class UnknownTagException(Exception): pass
 
 class BaseParser(object):
+    def __init__(self, inputEncoding=None, outputEncoding=None):
+        self.reset(inputEncoding, outputEncoding)
 
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
+    def reset(self, inputEncoding=None, outputEncoding=None):
         self.lastTag = None
         self.currentTag = None
         self.pieces = []
         self.line = 0
+        self.inputEncoding = True and inputEncoding or sys.getdefaultencoding()
+        self.outputEncoding = True and outputEncoding or sys.getdefaultencoding()
 
     def handleUnknownTag(self, tag, value):
         if value == ['']:
@@ -57,7 +59,12 @@ class BaseParser(object):
         self.__process(input)
 
     def __process(self, input):
-        lines = [self.parseLine(line) for line in input.read().split('\n')]
+        data = input.read()
+        if self.inputEncoding:
+            data = self.decode(data)
+
+        lines = [self.parseLine(line) for line in data.split('\n')]
+
         for tag, value in lines:
             self.handleTag(tag, value)
 
@@ -72,18 +79,27 @@ class BaseParser(object):
     def do_EmptyLine(self):
         pass
 
+    def output(self):
+        pass
+
+    def decode(self, str):
+        return str.decode(self.inputEncoding)
+
+    def encode(self, str):
+        return str.encode(self.outputEncoding)
 
 
+# TODO: add level-wise keyword discovery
 class SpaceParser(BaseParser):
     DOCUMENT_END = '======================================================================'
     SUBDOCUMENT_END = '----------------------------------------------------------------------'
     EMPTY_LINE = ''
     SUBDOCUMENT_KEY = 'SUBDOCUMENT'
 
-    def __self__(self):
-        BaseParser.__init__(self)
+    def __self__(self, inputEncoding=None, outputEncoding=None):
+        BaseParser.__init__(self, inputEncoding, outputEncoding)
 
-    def reset(self):
+    def reset(self, inputEncoding=None, outputEncoding=None):
         self.documents = []
         self.currentDocument = None
         self.currentSubdocument = None
@@ -92,9 +108,16 @@ class SpaceParser(BaseParser):
         self.current0 = None
         self.current5 = None
         self.current9 = None
-        BaseParser.reset(self)
+        self.keys = {}
+        BaseParser.reset(self, inputEncoding, outputEncoding)
 
         self.jointLines = 0
+
+    def handleTag(self, tag, value):
+        BaseParser.handleTag(self, tag, value)
+        if not tag in self.keys:
+            self.keys[tag] = set()
+        self.keys[tag].add(value[0])
 
     def parseLine(self, line):
         tag = len(line) - len(line.lstrip())
@@ -136,7 +159,6 @@ class SpaceParser(BaseParser):
             self.currentEntity = {}
 
         self.currentEntity[value[0]] = value[1]
-        # self.currentSubdocument[self.current5][value[0]] = value[1]
 
 
     def do_12(self, value):
@@ -199,21 +221,19 @@ class SpaceParser(BaseParser):
         else:
             for k, v in doc.items():
                 if type(v) == dict:
-                    print '%s%s:' %(indent, k)
+                    print '%s%s:' %(indent, self.encode(k))
                     self.printDocument(v, indent + '\t')
                 elif type(v) == list:
-                    print '%s%s:' %(indent, k)
+                    print '%s%s:' %(indent, self.encode(k))
                     # indent += '\t'
                     # for entry in v:
                     #     self.printDocument(entry, indent)
                     self.printDocument(v, indent + '\t', enumerate)
                 else:
-                    print '%s%s:%s' %(indent, k, v)
+                    print '%s%s:%s' %(indent, self.encode(k), self.encode(v))
 
 
 if __name__ == '__main__':
-    import sys
-    parser = SpaceParser()
+    parser = SpaceParser(inputEncoding='WINDOWS-1250', outputEncoding='utf-8')
     parser.feed(open(sys.argv[1]).read())
     parser.printDocument(parser.documents)
-    # print parser.jointLines
